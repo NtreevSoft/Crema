d07bd4754f0e0e6ee7fff16082d27358c35c4ac0
s2quake <s2quake@ntreev.com>
2018-04-19 19:27:44 +0900

    자바스크립트 타입 생성 관련 메소드 일부 추가 api 목록 작성시 메소드 반환 타입의 이름을 설정할 수 있는 attribute 추가

---
+++ common/Ntreev.Crema.Javascript.Sharing/Methods/AddTypeMemberMethod.cs
@@ -0,0 +1,55 @@
+﻿using Ntreev.Crema.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Text;
+
+namespace Ntreev.Crema.Javascript.Methods
+{
+    [Export(typeof(IScriptMethod))]
+    [PartCreationPolicy(CreationPolicy.NonShared)]
+    class AddTypeMemberMethod : DataBaseScriptMethodBase
+    {
+        [ImportingConstructor]
+        public AddTypeMemberMethod(ICremaHost cremaHost)
+            : base(cremaHost)
+        {
+
+        }
+
+        protected override Delegate CreateDelegate()
+        {
+            return new Action<string, string, long, string>(this.AddTypeMember);
+        }
+
+        private void AddTypeMember(string domainID, string name, long value, string comment)
+        {
+            if (domainID == null)
+                throw new ArgumentNullException(nameof(domainID));
+
+            if (this.CremaHost.GetService(typeof(IDomainContext)) is IDomainContext domainContext)
+            {
+                if (Guid.TryParse(domainID, out var guid) == true)
+                {
+                    var domain = domainContext.Dispatcher.Invoke(() => domainContext.Domains[guid]);
+                    if (domain.Host is ITypeTemplate template)
+                    {
+                        var authentication = this.Context.GetAuthentication(this);
+                        template.Dispatcher.Invoke(() =>
+                        {
+                            var typeMember = template.AddNew(authentication);
+                            typeMember.SetName(authentication, name);
+                            typeMember.SetValue(authentication, value);
+                            if (comment != null)
+                                typeMember.SetComment(authentication, comment);
+                            template.EndNew(authentication, typeMember);
+                        });
+                        return;
+                    }
+                }
+                throw new ArgumentException("Invalid domainID", nameof(domainID));
+            }
+            throw new NotImplementedException();
+        }
+    }
+}
