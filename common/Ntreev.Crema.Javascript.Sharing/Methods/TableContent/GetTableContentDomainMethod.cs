f2397729e9e29a205692a69df2a03c9b41b4675f
s2quake <s2quake@ntreev.com>
2018-04-27 15:46:19 +0900

    자바스크립트 메소드 추가

---
+++ common/Ntreev.Crema.Javascript.Sharing/Methods/TableContent/GetTableContentDomainMethod.cs
@@ -0,0 +1,39 @@
+﻿using Ntreev.Crema.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.Composition;
+using System.Text;
+
+namespace Ntreev.Crema.Javascript.Methods.TableContent
+{
+    [Export(typeof(IScriptMethod))]
+    [PartCreationPolicy(CreationPolicy.NonShared)]
+    [Category(nameof(TableContent))]
+    class GetTableContentDomainMethod : DataBaseScriptMethodBase
+    {
+        [ImportingConstructor]
+        public GetTableContentDomainMethod(ICremaHost cremaHost)
+            : base(cremaHost)
+        {
+
+        }
+
+        protected override Delegate CreateDelegate()
+        {
+            return new Func<string, string, string>(this.GetTableContentDomain);
+        }
+
+        [ReturnParameterName("domainID")]
+        private string GetTableContentDomain(string dataBaseName, string tableName)
+        {
+            var table = this.GetTable(dataBaseName, tableName);
+            return table.Dispatcher.Invoke(() =>
+            {
+                if (table.Content.Domain != null)
+                    return $"{table.Content.Domain.ID}";
+                return null;
+            });
+        }
+    }
+}
