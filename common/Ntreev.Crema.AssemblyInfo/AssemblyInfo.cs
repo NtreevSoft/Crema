e16c8bee7c7fba50d1fce238edebe92f9d5fe2ff
s2quake <s2quake@ntreev.com>
2018-06-25 11:12:53 +0900

    3.6.18176.1050 버전 출력이 잘못됨

--- common/Ntreev.Crema.AssemblyInfo/AssemblyInfo.cs
+++ common/Ntreev.Crema.AssemblyInfo/AssemblyInfo.cs
@@ -1,1 +1,13 @@
-﻿
+﻿using System.Reflection;
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+[assembly: AssemblyCompany("NtreevSoft")]
+[assembly: AssemblyCopyright("Copyright © Ntreev Soft 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("3.6")]
+[assembly: AssemblyFileVersion("3.6.18176.1050")]
+[assembly: AssemblyInformationalVersion("3.6.18176.1050")]
