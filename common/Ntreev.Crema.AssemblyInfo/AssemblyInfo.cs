15b7a08506e67e03ccfe4f219c6ffeddd49e6124
s2quake <s2quake@ntreev.com>
2018-07-23 14:51:12 +0900

    4.0.18159.1437

--- common/Ntreev.Crema.AssemblyInfo/AssemblyInfo.cs
+++ common/Ntreev.Crema.AssemblyInfo/AssemblyInfo.cs
@@ -26,5 +26,5 @@ using System.Reflection;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: AssemblyVersion("4.0")]
-[assembly: AssemblyFileVersion("4.0.18159.1437")]
-[assembly: AssemblyInformationalVersion("4.0.18159.1437")]
+[assembly: AssemblyFileVersion("4.0.18204.1450")]
+[assembly: AssemblyInformationalVersion("4.0.18204.1450")]
