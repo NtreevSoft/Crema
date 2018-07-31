c62cf13254a241b70864af60b0ea611ca9a13348
powerumc <umc@ntreev.com>
2018-07-31 15:45:39 +0900

    3.6.18212.1545 테이블 편집 창에서 검색 이후에 창을 닫으면 발생하는 에러 수정

--- common/Ntreev.Crema.AssemblyInfo/AssemblyInfo.cs
+++ common/Ntreev.Crema.AssemblyInfo/AssemblyInfo.cs
@@ -9,5 +9,5 @@
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: AssemblyVersion("3.6")]
-[assembly: AssemblyFileVersion("3.6.18211.1611")]
-[assembly: AssemblyInformationalVersion("3.6.18211.1611")]
+[assembly: AssemblyFileVersion("3.6.18212.1545")]
+[assembly: AssemblyInformationalVersion("3.6.18212.1545")]
