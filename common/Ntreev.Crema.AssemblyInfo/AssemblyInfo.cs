acc473d96c29b2645175c7ff790a07357433273b
엄 준일 <umc@ntreev.com>
2018-12-04 14:08:04 +0900

    3.6.18338.1407

--- common/Ntreev.Crema.AssemblyInfo/AssemblyInfo.cs
+++ common/Ntreev.Crema.AssemblyInfo/AssemblyInfo.cs
@@ -9,5 +9,5 @@
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: AssemblyVersion("3.6")]
-[assembly: AssemblyFileVersion("3.6.18332.1453")]
-[assembly: AssemblyInformationalVersion("3.6.18332.1453")]
+[assembly: AssemblyFileVersion("3.6.18338.1407")]
+[assembly: AssemblyInformationalVersion("3.6.18338.1407")]
