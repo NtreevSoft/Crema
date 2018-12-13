cdaab132c9dbb97d889b1a59e6ad24df99c3cc92
admin <>
2018-12-13 15:21:29 +0900

    3.6.18347.1521

--- common/Ntreev.Crema.AssemblyInfo/AssemblyInfo.cs
+++ common/Ntreev.Crema.AssemblyInfo/AssemblyInfo.cs
@@ -9,5 +9,5 @@
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: AssemblyVersion("3.6")]
-[assembly: AssemblyFileVersion("3.6.18338.1407")]
-[assembly: AssemblyInformationalVersion("3.6.18338.1407")]
+[assembly: AssemblyFileVersion("3.6.18347.1521")]
+[assembly: AssemblyInformationalVersion("3.6.18347.1521")]
