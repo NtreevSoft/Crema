a6d238d1620f0969be927642c3aeda7ef74958c2
powerumc <umc@ntreev.com>
2019-01-21 12:02:18 +0900

    gen-ver.ps1 의 날짜 랜더링을 "D3" 로 변경

--- common/Ntreev.Crema.AssemblyInfo/AssemblyInfo.cs
+++ common/Ntreev.Crema.AssemblyInfo/AssemblyInfo.cs
@@ -9,5 +9,5 @@
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: AssemblyVersion("3.6")]
-[assembly: AssemblyFileVersion("3.6.1917.1019")]
-[assembly: AssemblyInformationalVersion("3.6.1917.1019")]
+[assembly: AssemblyFileVersion("3.6.19021.1158")]
+[assembly: AssemblyInformationalVersion("3.6.19021.1158")]
