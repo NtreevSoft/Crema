ae4571fedbf2e0eece816c9aa57ab96fb3839963
powerumc <powerumc@gmail.com>
2019-05-27 14:42:09 +0900

    [tool] 필터링된 테이블 목록을 파일별로 출력하기 위해 기능
    사용 방법
    - .\cremadev.exe get-data localhost:4004 split-test --database GameData_KOR_Dev --split

---
+++ tools/Ntreev.Crema.Commands/DataSplitSetting.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ntreev.Library.Commands;
+
+namespace Ntreev.Crema.Commands
+{
+    static class DataSplitSetting
+    {
+        [CommandProperty]
+        [Description("크레마 데이터 파일을 테이블 이름별로 출력합니다.\n이 옵션을 사용하면 필수인자 <filename> 은 출력할 디렉토리로 사용합니다.")]
+        [DefaultValue(false)]
+        public static bool Split
+        {
+            get; set;
+        }
+    }
+}
