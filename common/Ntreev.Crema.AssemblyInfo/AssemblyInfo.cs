233256fb22ec25bbf141afbdae200b5ed5657ae2
powerumc <powerumc@gmail.com>
2019-08-26 18:02:39 +0900

    3.6.19238.1802

--- common/Ntreev.Crema.AssemblyInfo/AssemblyInfo.cs
+++ common/Ntreev.Crema.AssemblyInfo/AssemblyInfo.cs
@@ -9,5 +9,5 @@
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: AssemblyVersion("3.6")]
-[assembly: AssemblyFileVersion("3.6.19238.1448")]
-[assembly: AssemblyInformationalVersion("3.6.19238.1448")]
+[assembly: AssemblyFileVersion("3.6.19238.1802")]
+[assembly: AssemblyInformationalVersion("3.6.19238.1802")]
