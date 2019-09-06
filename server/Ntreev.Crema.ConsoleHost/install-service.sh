b38c449c24e9afc1bfc93f96f5ca6f93255da462
powerumc <powerumc@gmail.com>
2019-09-06 10:28:55 +0900

    [server] 크레마 서비스 설치 스크립트 추가
    ### 제한사항
    아직 크레마 서버는 리눅스 계열의 운영체제에서 처리하는 SIGTERM 이벤트 수신이 구현되지 않았습니다.
    
    ### 윈도우 서비스 설치 및 제거
    ```bash
    .\install-service.cmd <repo-path>
    .\uninstall-service.cmd <repo-path>
    ```
    
    ### 우분투, 센트OS, 맥OS
    관리자 권한으로 실행해야 합니다.
    ```bash
    ./install-service.sh <repo-path>
    ./uninstall-service.sh
    ```bash

---
+++ server/Ntreev.Crema.ConsoleHost/install-service.sh
@@ -0,0 +1,80 @@
+#!/usr/bin/env bash
+
+CREMA_EXECUTION_FILENAME="cremaserver.exe"
+OPTION_REPO_DIR="$1"
+
+validate() {
+    local HAS_ERROR=false
+
+    if ! [ `command -v svn` ]; then
+        echo "ERROR: 'svn' is not installed."
+        HAS_ERROR=true
+    fi
+
+    if ! [ `command -v mono` ]; then
+        echo "ERROR: 'mono' is not installed."
+        HAS_ERROR=true
+    fi
+
+    if ! [ -f "$CREMA_EXECUTION_FILENAME" ]; then
+        echo "ERROR: '$CREMA_EXECUTION_FILENAME' file is not exists."
+        HAS_ERROR=true
+    fi
+    
+    if $HAS_ERROR; then
+        exit 1
+    fi
+}
+
+install() {
+    content="$(cat install-service.linux.template)"
+    content="${content/\{pwd\}/`pwd`}"
+    content="${content/\{repo-dir\}/\"$OPTION_REPO_DIR\"}"
+    echo "$content" > /etc/systemd/system/crema.service
+    if [ $? != 0 ]; then
+        echo "ERROR: Could not write file. (etc/systemd/system/crema.service)"
+        exit 1
+    fi
+
+    systemctl enable crema.service
+    systemctl start crema.service
+}
+
+install_on_mac() {
+    content="$(cat install-service.macos.template)"
+    content="${content/\{pwd\}/`pwd`}"
+    content="${content/\{repo-dir\}/\"$OPTION_REPO_DIR\"}"
+    echo "$content" > crema.plist
+    if [ $? != 0 ]; then
+        echo "ERROR: Could not write file. (crema.plist)"
+        exit 1
+    fi
+
+    launchctl load -w ./crema.plist
+    launchctl start crema
+}
+
+help() {
+    echo "Usage"
+    echo " $0 <crema-repo-path>"
+}
+
+if [ -z "$OPTION_REPO_DIR" ]; then
+    help
+    exit
+fi
+
+osname="$(uname -a)"
+case "$osname" in
+    *Linux*)
+        install
+        ;;
+    *Darwin*)
+        install_on_mac
+        ;;
+    *)
+        echo "ERROR: Not supports os. ('$osname')"
+        exit 1
+    ;;
+    
+esac
