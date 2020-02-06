c27c6bc0d19d9716fe6b80325905f6d7581df40b
powerumc <powerumc@gmail.com>
2020-02-06 16:48:58 +0900

    3.7.20037.1648

--- common/Ntreev.Crema.AssemblyInfo/AssemblyInfo.cs
+++ common/Ntreev.Crema.AssemblyInfo/AssemblyInfo.cs
@@ -9,5 +9,5 @@
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: AssemblyVersion("3.7")]
-[assembly: AssemblyFileVersion("3.7.20036.1328")]
-[assembly: AssemblyInformationalVersion("3.7.20036.1328")]
+[assembly: AssemblyFileVersion("3.7.20037.1648")]
+[assembly: AssemblyInformationalVersion("3.7.20037.1648")]
