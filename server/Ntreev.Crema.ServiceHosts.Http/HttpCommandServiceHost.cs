33361764a879412696756cbf9794b8242775fb16
powerumc <powerumc@gmail.com>
2020-04-27 11:22:34 +0900

    [server] 크레마 서버 크래시 현상 수정
    Mono 런타임의 `HttpListener.GetContextAsync()` 에서 Response 를 Disposed 한 후 Owin 서버가 Response.StatusCode setter 에 엑세스하려고 하여 발생하는 오류이다.
    
    Mono 런타임을 사용하는 HTTP 서버인 경우 아래의 조건에 의해 크래시가 발생한다.
    1. 'POST', 'PUT' HTTP Method 인 경우
    2. HTTP Body 의 Payload 가 없는 경우
    3. 'Content-Length' HTTP Header 가 없는 경우

--- server/Ntreev.Crema.ServiceHosts.Http/HttpCommandServiceHost.cs
+++ server/Ntreev.Crema.ServiceHosts.Http/HttpCommandServiceHost.cs
@@ -15,6 +15,8 @@ namespace Ntreev.Crema.ServiceHosts.Http
         private readonly ICremaHost cremaHost;
         private readonly CremaService cremaService;
         private IDisposable server;
+        
+        private static readonly bool IsMono = Type.GetType("Mono.Runtime") != null;
 
         [ImportingConstructor]
         public HttpCommandServiceHost(ICremaHost cremaHost, CremaService cremaService)
@@ -25,7 +27,12 @@ namespace Ntreev.Crema.ServiceHosts.Http
 
         public void Open(int port)
         {
-            this.server = WebApp.Start($"http://*:{port}", app =>
+            var option = new StartOptions
+            {
+                Urls = { $"http://*:{port}" },
+                ServerFactory = IsMono ? typeof(OwinServerFactory).Namespace : null
+            };
+            this.server = WebApp.Start(option, app =>
             {
                 var config = new HttpConfiguration();
                 config.ConfigureCrema(this.cremaHost)
