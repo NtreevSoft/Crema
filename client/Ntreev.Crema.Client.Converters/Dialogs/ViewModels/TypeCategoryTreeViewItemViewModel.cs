b927df27ad2e7243aa22f5ce289d6b2a7cbd09e4
powerumc <powerumc@gmail.com>
2020-05-07 15:09:21 +0900

    [client] 타입을 엑셀로 내보내는 기능 추가

---
+++ client/Ntreev.Crema.Client.Converters/Dialogs/ViewModels/TypeCategoryTreeViewItemViewModel.cs
@@ -0,0 +1,67 @@
+﻿//Released under the MIT License.
+//
+//Copyright (c) 2018 Ntreev Soft co., Ltd.
+//
+//Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
+//documentation files (the "Software"), to deal in the Software without restriction, including without limitation the 
+//rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit 
+//persons to whom the Software is furnished to do so, subject to the following conditions:
+//
+//The above copyright notice and this permission notice shall be included in all copies or substantial portions of the 
+//Software.
+//
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE 
+//WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR 
+//COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR 
+//OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+using System;
+using Ntreev.Crema.Services;
+using Ntreev.Crema.ServiceModel;
+using Ntreev.Crema.Client.Framework;
+using Ntreev.Crema.Data;
+using System.Threading.Tasks;
+
+namespace Ntreev.Crema.Client.Converters.Dialogs.ViewModels
+{
+    public class TypeCategoryTreeViewItemViewModel : ExportTreeViewItemViewModel
+    {
+        private readonly Authentication authentication;
+        private readonly TypeCategoryDescriptor descriptor;
+
+        public TypeCategoryTreeViewItemViewModel(Authentication authentication, TypeCategoryDescriptor descriptor)
+        {
+            this.authentication = authentication;
+            this.descriptor = descriptor;
+
+            foreach (var item in descriptor.Categories)
+            {
+                var viewModel = new TypeCategoryTreeViewItemViewModel(authentication, item)
+                {
+                    Parent = this
+                };
+            }
+
+            foreach (var item in descriptor.Types)
+            {
+                var viewModel = new TypeTreeViewItemViewModel(authentication, item)
+                {
+                    Parent = this
+                };
+            }
+        }
+
+        public override Task PreviewAsync(CremaDataSet dataSet)
+        {
+            throw new NotSupportedException();
+        }
+
+        public override string DisplayName => this.Name;
+
+        public override bool CanCheck => this.descriptor.AccessType >= AccessType.Guest;
+
+        public override string Path => this.descriptor.Path;
+
+        public string Name => this.descriptor.Name;
+    }
+}
---
+++ client/Ntreev.Crema.Client.Converters/MenuItems/ExportTypeMenuItem.cs
@@ -0,0 +1,53 @@
+﻿//Released under the MIT License.
+//
+//Copyright (c) 2018 Ntreev Soft co., Ltd.
+//
+//Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
+//documentation files (the "Software"), to deal in the Software without restriction, including without limitation the 
+//rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit 
+//persons to whom the Software is furnished to do so, subject to the following conditions:
+//
+//The above copyright notice and this permission notice shall be included in all copies or substantial portions of the 
+//Software.
+//
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE 
+//WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR 
+//COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR 
+//OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+using System.ComponentModel.Composition;
+using Ntreev.Crema.Client.Converters.Dialogs.ViewModels;
+using Ntreev.Crema.Client.Converters.Properties;
+using Ntreev.Crema.Client.Framework;
+using Ntreev.ModernUI.Framework;
+
+namespace Ntreev.Crema.Client.Converters.MenuItems
+{
+    [Export(typeof(IMenuItem))]
+    [ParentType(typeof(IToolMenuItem))]
+    class ExportTypeMenuItem : MenuItemBase
+    {
+        [Import] private Authenticator authenticator = null;
+        private ICremaAppHost cremaAppHost;
+
+        [ImportingConstructor]
+        public ExportTypeMenuItem(ICremaAppHost cremaAppHost)
+        {
+            this.cremaAppHost = cremaAppHost;
+            this.cremaAppHost.Loaded += InvokeCanExecuteChangedEvent;
+            this.cremaAppHost.Unloaded += InvokeCanExecuteChangedEvent;
+            this.DisplayName = Resources.MenuItem_ExportType;
+        }
+
+        protected override async void OnExecute(object parameter)
+        {
+            var dialog = await ExportTypeViewModel.CreateInstanceAsync(this.authenticator, this.cremaAppHost);
+            dialog?.ShowDialog();
+        }
+
+        protected override bool OnCanExecute(object parameter)
+        {
+            return this.cremaAppHost.IsLoaded;
+        }
+    }
+}
