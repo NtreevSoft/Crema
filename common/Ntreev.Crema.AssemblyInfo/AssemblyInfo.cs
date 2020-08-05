8ec876bf90c40faaca2a2bbb5c28966f48d76062
powerumc <powerumc@gmail.com>
2020-08-05 11:08:33 +0900

    3.6.20218.1108

--- common/Ntreev.Crema.AssemblyInfo/AssemblyInfo.cs
+++ common/Ntreev.Crema.AssemblyInfo/AssemblyInfo.cs
@@ -9,5 +9,5 @@
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: AssemblyVersion("3.6")]
-[assembly: AssemblyFileVersion("3.6.20216.1051")]
-[assembly: AssemblyInformationalVersion("3.6.20216.1051")]
+[assembly: AssemblyFileVersion("3.6.20218.1108")]
+[assembly: AssemblyInformationalVersion("3.6.20218.1108")]
